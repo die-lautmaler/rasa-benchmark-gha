71a75b700a605a682490da3a7b94ca2b57d866d3
konrad <brueggemann4@uni-potsdam.de>
2024-10-01 17:06:35 +0200

    fix ftype

--- benchmark/main.py
+++ benchmark/main.py
@@ -14,13 +14,14 @@ def main(threshold: str, nlu_data_dir: str):
     else:
         filepath = nlu_data_dir
         nlu_data_dir = os.path.dirname(filepath)
-    ftype = nlu_data_dir.split(".")[-1] # get file type from nlu_data_dir
+    # get file type from nlu_data_dir
+    ftype = filepath.split(".")[-1]
     nlu_benchmark.check(threshold=threshold, nlu_data_dir=nlu_data_dir, ftype=ftype)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run NLU benchmark")
     parser.add_argument("--threshold", type=str, help="Threshold for NLU benchmark")
-    parser.add_argument("--nlu-data-dir", type=str, help="NLU data directory")
+    parser.add_argument("--nlu-data", type=str, help="NLU data directory")
     args = parser.parse_args()
-    main(args.threshold, args.nlu_data_dir)
+    main(args.threshold, args.nlu_data)
